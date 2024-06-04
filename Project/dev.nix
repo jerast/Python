e019316558025abb7def0a06699d87cbee41d9af
Jerast <jerast@github.com>
2024-06-04 18:24:22 -0500

    [update] Project : add dev.nix file for IDX development

---
+++ Project/dev.nix
@@ -0,0 +1,65 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go 
+    # pkgs.curl
+    pkgs.python310
+    # pkgs.unixODBC
+    # pkgs.unixODBCDrivers.msodbcsql18
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "ms-python.debugpy"
+      "ms-python.python"
+      "mechatroner.rainbow-csv"
+      "ms-toolsai.jupyter"
+      "ms-toolsai.jupyter-keymap"
+      "ms-toolsai.jupyter-renderers"
+      "ms-toolsai.vscode-jupyter-cell-tags"
+      "ms-toolsai.vscode-jupyter-slideshow"
+    ];
+    # Enable previews
+    previews = {
+      enable = false;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        create-venv = ''
+          python -m venv .venv
+          source .venv/bin/activate
+          pip install -r code/requirements.txt
+        '';
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # start-venv = "source .venv/bin/activate";
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
